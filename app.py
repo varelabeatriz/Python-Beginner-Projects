46665f870699687aef265e6ae06927e895898e0b
Beatriz Varela <beatrizsv19@gmail.com>
2023-01-14 12:52:26 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import random
+
+def play():
+    user = input("'r' for rock, 'p' for paper, 's' for scissors")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'tie'
+
+    if is_win(user, computer):
+        return 'user wins'
+    else:
+        return 'user loses'
+    
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or \
+    (player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
